6b7fcd13a69dda777d0228bdca80198ea4d44b81
On1zuma <wassi.mess@hotmail.com>
2024-02-08 09:31:15 +0100

    FEAT: #1 Create

---
+++ EasySave/EasySave/EditSave.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave
+{
+    internal class EditSave
+    {
+        public static void Create(string sourceFile, string destinationDirectory)
+        {
+            string formattedDateTime = DateTime.Now.ToString("MM-dd-yyyy-h-mm-ss");
+            Console.WriteLine("DATE: " + formattedDateTime);
+            // Guid.NewGuid().ToString("N")
+            string pathWithId = Path.Combine(destinationDirectory, System.IO.Path.ChangeExtension(Path.GetFileName(sourceFile), null) + "-" + formattedDateTime);
+            Console.WriteLine("PATH WITH ID" + pathWithId);
+            bool directoryExists = Directory.Exists(pathWithId);
+
+            Console.WriteLine("Already exist: " + directoryExists);
+
+            // TODO: destinationFile HAVE TO BE SAVED IN THE DB
+            string destinationFile = Path.Combine(pathWithId, Path.GetFileName(sourceFile));
+
+            Console.WriteLine("FINAL DEST" + destinationFile);
+            try
+            {
+                if (directoryExists == false)
+                {
+                    System.IO.Directory.CreateDirectory(pathWithId);
+                    System.IO.File.Copy(sourceFile, destinationFile);
+                    Console.WriteLine("File copied successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Already exist");
+                }
+            }
+            catch (IOException iox)
+            {
+                Console.WriteLine("Error: " + iox.Message);
+            }
+        }
+
+
+
+    }
+}
