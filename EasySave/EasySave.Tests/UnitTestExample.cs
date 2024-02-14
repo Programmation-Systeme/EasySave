731e9cabf721d51d21797816bfa9134a2cec0a1d
On1zuma <wassi.mess@hotmail.com>
2024-02-14 14:19:20 +0100

    qa: testing when test don't work

--- EasySave/EasySave.Tests/UnitTestExample.cs
+++ EasySave/EasySave.Tests/UnitTestExample.cs
@@ -19,7 +19,7 @@ namespace EasySave.Tests
             bool fileSaved = false;
 
             // Assert that the file has not been saved
-            Assert.False(fileSaved);
+            Assert.True(fileSaved);
         }
     }
 }
