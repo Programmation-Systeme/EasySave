2a845c60635a1a9e4fdf6b23a8633b5958f236d1
On1zuma <wassi.mess@hotmail.com>
2024-02-14 14:16:44 +0100

    qa: new test

--- EasySave/EasySave.Tests/UnitTestExample.cs
+++ EasySave/EasySave.Tests/UnitTestExample.cs
@@ -11,5 +11,15 @@ namespace EasySave.Tests
             // Assert that the file has been saved
             Assert.True(fileSaved);
         }
+
+        [Fact]
+        public void saveFile_WhenFileHasNotBeenSaved_ReturnsFalse_Example()
+        {
+            // Simulate a file not being saved
+            bool fileSaved = false;
+
+            // Assert that the file has not been saved
+            Assert.False(fileSaved);
+        }
     }
 }
