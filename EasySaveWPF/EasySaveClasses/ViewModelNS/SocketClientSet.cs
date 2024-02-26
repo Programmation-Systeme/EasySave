da54b9479e9af199439d236538f4b8270a75a831
On1zuma <wassi.mess@hotmail.com>
2024-02-26 11:29:43 +0100

    qa: comments for client

--- EasySaveWPF/EasySaveClasses/ViewModelNS/SocketClientSet.cs
+++ EasySaveWPF/EasySaveClasses/ViewModelNS/SocketClientSet.cs
@@ -6,46 +6,94 @@ namespace EasySaveClasses.ViewModelNS
 {
     internal class SocketClientSet
     {
+        /// <summary>
+        /// Launches the client, connects to the server, sends user choice, and receives response.
+        /// </summary>
+        /// <param name="args">Command-line arguments (not used in this method).</param>
+        /// <param name="choice">User's choice to send to the server.</param>
+        /// <returns>Response received from the server.</returns>
         public static string LaunchClient(string[] args, string choice)
         {
+            // Informing the user that the client is starting
             Console.WriteLine("Démarrage du client...");
+
+            // Establishing a connection with the server
             Socket clientSocket = Connection();
+
+            // Connection successful message
             Console.WriteLine("Connecté au serveur !");
+
+            // Sending user's choice to the server and receiving response
             string clientResponse = ListenToNetwork(clientSocket, choice);
+
+            // Closing the connection to the server
             LogOff(clientSocket);
 
+            // Returning the response received from the server
             return clientResponse;
         }
 
+        /// <summary>
+        /// Establishes a socket connection to the server.
+        /// </summary>
+        /// <returns>Connected Socket object.</returns>
         private static Socket Connection()
         {
+            // Server's IP address (localhost in this case)
             IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+            // Server's endpoint
             IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8080);
+
+            // Creating a socket using TCP protocol
             Socket clientSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Connecting the socket to the server
             clientSocket.Connect(remoteEP);
+
+            // Returning the connected socket
             return clientSocket;
         }
 
+        /// <summary>
+        /// Sends user's choice to the server and receives a response.
+        /// </summary>
+        /// <param name="clientSocket">Connected Socket object.</param>
+        /// <param name="choice">User's choice to send to the server.</param>
+        /// <returns>Response received from the server.</returns>
         private static string ListenToNetwork(Socket clientSocket, string choice)
         {
+            // Buffer to hold incoming data
             byte[] buffer = new byte[1024];
             int bytesRead;
 
+            // Sending the user's choice to the server
             Console.Write("Client: ");
-            // string message = Console.ReadLine();
             clientSocket.Send(Encoding.ASCII.GetBytes(choice));
- 
+
+            // Receiving response from the server
             bytesRead = clientSocket.Receive(buffer);
+
+            // Converting the received bytes to a string
             string listSaves = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            // Displaying the received message from the server
             Console.WriteLine($"Server: {listSaves}");
 
+            // Returning the received message
             return listSaves;
-            
         }
 
+        /// <summary>
+        /// Closes the socket connection to the server.
+        /// </summary>
+        /// <param name="socket">Socket object to close.</param>
         private static void LogOff(Socket socket)
         {
+            // Shutdown the socket
             socket.Shutdown(SocketShutdown.Both);
+
+            // Close the socket
             socket.Close();
         }
 
